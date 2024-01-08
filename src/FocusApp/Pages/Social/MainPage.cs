af93ea5aa0716f060881a0ce8a6186ef457cf7ba
Eli Payton <80854811+wesleypayton@users.noreply.github.com>
2024-01-07 20:36:49 -0600

    Added title + pfp placeholder

--- src/FocusApp/Pages/Social/MainPage.cs
+++ src/FocusApp/Pages/Social/MainPage.cs
@@ -1,4 +1,8 @@
-﻿namespace FocusApp.Pages.Social;
+﻿using CommunityToolkit.Maui.Markup;
+using CommunityToolkit.Maui.Markup.LeftToRight;
+using Microsoft.Maui.Controls.Shapes;
+
+namespace FocusApp.Pages.Social;
 
 internal sealed class MainPage : BasePage
 {
@@ -14,12 +18,26 @@ internal sealed class MainPage : BasePage
             {
                 new Label
                 {
-                    Text = "Social",
-                    FontSize = 40,
-                    HorizontalOptions = LayoutOptions.Center,
-                    VerticalOptions = LayoutOptions.Center
+                    Text = "Friends",
+                    FontSize = 40
                 }
-            }
+                .Top()
+                .Left()
+                .Padding(15, 15),
+
+                new Image
+                {
+                    Source = new FileImageSource
+                    {
+                        File = "dotnet_bot.png",
+                    },
+                    WidthRequest = 90,
+                    HeightRequest =90
+                }
+                .Top()
+                .Right()
+                .Clip(new EllipseGeometry { Center = new Point(43, 45), RadiusX = 27, RadiusY = 27 })
+    }
         };
     }
 }
