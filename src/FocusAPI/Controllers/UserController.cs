35f3e984e80cc5374c39dc6eb4cee3bed6347a90
egray <evangray7373@gmail.com>
2024-01-09 20:59:45 -0800

    Implemented command and query flow

--- src/FocusAPI/Controllers/UserController.cs
+++ src/FocusAPI/Controllers/UserController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using FocusCore.Models;
 using MediatR;
 using FocusCore.Queries;
+using FocusCore.Commands;
 
 namespace FocusAPI.Controllers
 {
@@ -22,6 +23,12 @@ namespace FocusAPI.Controllers
         public async Task<UserModel> GetUserData()
         {
             return await _mediator.Send(new GetUserDataQuery { Id = Guid.NewGuid() });
+        }
+
+        [HttpPost]
+        public async Task CreateUser(CreateUserCommand command)
+        {
+            await _mediator.Send(command);
         }
     }
 }
---
+++ src/FocusCore/Methods/User/CreateUser.cs
@@ -0,0 +1,30 @@
+﻿using FocusCore.Commands;
+using FocusCore.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FocusCore.Methods.User;
+public class CreateUser
+{
+    public class Handler : IRequestHandler<CreateUserCommand, Unit>
+    {
+        // TODO: Add db client reference
+        public Handler() { }
+
+        public async Task<Unit> Handle(CreateUserCommand command, CancellationToken cancellationToken)
+        {
+            UserModel user = new UserModel
+            {
+                Name = command.Name
+            };
+
+            // TODO: Insert new user into database
+
+            return Unit.Value;
+        }
+    }
+}
