5f2f290fbf5a2de80a44c17103f187acf629e74c
egray <evangray7373@gmail.com>
2024-01-09 20:43:52 -0800

    Implemented MediatR, allowed references to FocusCore

--- src/FocusAPI/Program.cs
+++ src/FocusAPI/Program.cs
@@ -1,3 +1,6 @@
+using FocusCore.Methods.User;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure MediatR
+Assembly[] assemblies = new Assembly[] { Assembly.GetExecutingAssembly(), Assembly.GetAssembly(typeof(GetUserData)) };
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(assemblies));
 
 var app = builder.Build();
 
