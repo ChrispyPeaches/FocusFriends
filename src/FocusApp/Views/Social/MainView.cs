4b37a9b105b4839530677e5c46f32df8c1b6d11f
Eli Payton <80854811+wesleypayton@users.noreply.github.com>
2024-01-11 00:40:01 -0600

    Fixed formatting stuff from merge

--- src/FocusApp/Views/Social/MainView.cs
+++ src/FocusApp/Views/Social/MainView.cs
@@ -14,78 +14,79 @@ public class MainView : ContentView
         // Add logic to fetch focused friends
         List<ImageCell> focusingFriends = new List<ImageCell>();
         for (int i = 0; i < 5; i++)
-{
+        {
             focusingFriends.Add(new ImageCell
-    {
+            {
                 Text = "Friend" + i,
                 ImageSource = new FileImageSource
-        {
-                        // Add logic that gets profile picture from friend user data
-                        File = "dotnet_bot.png"
-                    },
+                {
+                    // Add logic that gets profile picture from friend user data
+                    File = "dotnet_bot.png"
+                },
                 BindingContext = this
             });
         };
 
-            Content = new Grid
-            {
+        Content = new Grid
+        {
             // Define rows and columns (Star means that row/column will take up the remaining space)
             RowDefinitions = Rows.Define(80, Star),
             ColumnDefinitions = Columns.Define(Star, Star),
+            BackgroundColor = Colors.LightGreen,
+            Opacity = 0.9,
 
-                Children =
+            Children =
+            {
+            // Header
+                new Label
                 {
-                // Header
-                    new Label
-                    {
-                    Text = "Friends",
-                    FontSize = 40
-                }
-                .Row(0)
-                .Column(0)
-                .Padding(15, 15),
+                Text = "Friends",
+                FontSize = 40
+            }
+            .Row(0)
+            .Column(0)
+            .Padding(15, 15),
 
-                // Horizontal Divider
-                new BoxView
-                {
-                    Color = Color.Parse("Black"),
-                    WidthRequest = 400,
-                    HeightRequest = 2
-                }
-                .Bottom()
-                .Row(0)
-                .Column(0)
-                .ColumnSpan(2),
+            // Horizontal Divider
+            new BoxView
+            {
+                Color = Color.Parse("Black"),
+                WidthRequest = 400,
+                HeightRequest = 2
+            }
+            .Bottom()
+            .Row(0)
+            .Column(0)
+            .ColumnSpan(2),
 
-                // Profile Picture
-                new Image
+            // Profile Picture
+            new Image
+            {
+                Source = new FileImageSource
                 {
-                    Source = new FileImageSource
-                    {
-                        // Add logic that gets profile picture from user data
-                        File = "dotnet_bot.png"
-                    },
-                    WidthRequest = 90,
-                    HeightRequest = 90
-                }
-                .Top()
-                .Right()
-                .Column(1)
-                .Clip(new EllipseGeometry { Center = new Point(43, 45), RadiusX = 27, RadiusY = 27 }),
+                    // Add logic that gets profile picture from user data
+                    File = "dotnet_bot.png"
+                },
+                WidthRequest = 90,
+                HeightRequest = 90
+            }
+            .Top()
+            .Right()
+            .Column(1)
+            .Clip(new EllipseGeometry { Center = new Point(43, 45), RadiusX = 27, RadiusY = 27 }),
 
-                // Friends List
-                new ListView()
-                {
-                    Header = "Focusing",
-                    ItemsSource = focusingFriends
-                    //ItemTemplate = new DataTemplate(typeof(ImageCell)),
-                    //BindingContext = ImageCell.BindingContextProperty
-                    }
-                .Row(1)
-                .Column(0)
-                .ColumnSpan(2)
-                }
-            };
-        }
+            // Friends List
+            new ListView()
+            {
+                Header = "Focusing",
+                ItemsSource = focusingFriends
+                //ItemTemplate = new DataTemplate(typeof(ImageCell)),
+                //BindingContext = ImageCell.BindingContextProperty
+            }
+            .Row(1)
+            .Column(0)
+            .ColumnSpan(2)
+            }
+        };
     }
 }
