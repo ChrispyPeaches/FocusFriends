663a7c0f84d1be360289192ff2f5fa0f65b0ce4a
Eli Payton <80854811+wesleypayton@users.noreply.github.com>
2024-01-11 01:59:40 -0600

    Fixed formatting and FIGURED OUT BINDING!

--- src/FocusApp/Views/Social/MainView.cs
+++ src/FocusApp/Views/Social/MainView.cs
@@ -17,7 +17,7 @@ public class MainView : ContentView
         {
             focusingFriends.Add(new ImageCell
             {
-                Text = "Friend" + i,
+                Text = "Friend " + i,
                 ImageSource = new FileImageSource
                 {
                     // Add logic that gets profile picture from friend user data
@@ -27,65 +27,68 @@ public class MainView : ContentView
             });
         };
 
+        DataTemplate dataTemplate = new DataTemplate(typeof(ImageCell));
+        dataTemplate.SetBinding(ImageCell.TextProperty, "Text");
+        dataTemplate.SetBinding(ImageCell.ImageSourceProperty, "ImageSource");
+
         Content = new Grid
         {
             // Define rows and columns (Star means that row/column will take up the remaining space)
-            RowDefinitions = Rows.Define(80, Star),
+            RowDefinitions = Rows.Define(80, Star, Star, Star),
             ColumnDefinitions = Columns.Define(Star, Star),
             BackgroundColor = Colors.LightGreen,
             Opacity = 0.9,
 
             Children =
             {
-            // Header
+                // Header
                 new Label
                 {
-                Text = "Friends",
-                FontSize = 40
-            }
-            .Row(0)
-            .Column(0)
-            .Padding(15, 15),
+                    Text = "Friends",
+                    FontSize = 40
+                }
+                .Row(0)
+                .Column(0)
+                .Padding(15, 15),
 
-            // Horizontal Divider
-            new BoxView
-            {
-                Color = Color.Parse("Black"),
-                WidthRequest = 400,
-                HeightRequest = 2
-            }
-            .Bottom()
-            .Row(0)
-            .Column(0)
-            .ColumnSpan(2),
-
-            // Profile Picture
-            new Image
-            {
-                Source = new FileImageSource
+                // Horizontal Divider
+                new BoxView
                 {
-                    // Add logic that gets profile picture from user data
-                    File = "dotnet_bot.png"
-                },
-                WidthRequest = 90,
-                HeightRequest = 90
-            }
-            .Top()
-            .Right()
-            .Column(1)
-            .Clip(new EllipseGeometry { Center = new Point(43, 45), RadiusX = 27, RadiusY = 27 }),
+                    Color = Color.Parse("Black"),
+                    WidthRequest = 400,
+                    HeightRequest = 2
+                }
+                .Bottom()
+                .Row(0)
+                .Column(0)
+                .ColumnSpan(2),
 
-            // Friends List
-            new ListView()
-            {
-                Header = "Focusing",
-                ItemsSource = focusingFriends
-                //ItemTemplate = new DataTemplate(typeof(ImageCell)),
-                //BindingContext = ImageCell.BindingContextProperty
-            }
-            .Row(1)
-            .Column(0)
-            .ColumnSpan(2)
+                // Profile Picture
+                new Image
+                {
+                    Source = new FileImageSource
+                    {
+                        // Add logic that gets profile picture from user data
+                        File = "dotnet_bot.png"
+                    },
+                    WidthRequest = 90,
+                    HeightRequest = 90
+                }
+                .Top()
+                .Right()
+                .Column(1)
+                .Clip(new EllipseGeometry { Center = new Point(43, 45), RadiusX = 27, RadiusY = 27 }),
+
+                // Friends List
+                new ListView
+                {
+                    Header = "Focusing",
+                    ItemsSource = focusingFriends,
+                    ItemTemplate = dataTemplate
+                }
+                .Row(1)
+                .Column(0)
+                .ColumnSpan(2)
             }
         };
     }
