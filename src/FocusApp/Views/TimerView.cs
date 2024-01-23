4bc0053d6da73f0b8e1448154f07fd078bf06573
ChrispyPeaches <gameboy9478@gmail.com>
2024-01-23 00:39:44 -0600

    Add Timer increment and decrement buttons

--- src/FocusApp/Views/TimerView.cs
+++ src/FocusApp/Views/TimerView.cs
@@ -1,17 +1,35 @@
 ﻿using CommunityToolkit.Maui.Markup;
 using CommunityToolkit.Maui.Markup.LeftToRight;
+using FocusApp.Helpers;
 using FocusApp.Resources.FontAwesomeIcons;
-using Sharpnado.Tabs;
 
 namespace FocusApp.Views
 {
     internal class TimerView : ContentView
     {
+
+        enum Row { TopBar, TimerDisplay, Island, TimerButtons, BottomWhiteSpace }
+        enum Column { LeftTimerButton, TimerAmount, RightTimerButton }
+
         public TimerView()
         {
+            _timerHelper = new TimerHelper();
+
             Content = new Grid
             {
-                BackgroundColor = Colors.PeachPuff,
+                RowDefinitions = GridRowsColumns.Rows.Define(
+                    ( Row.TopBar, GridRowsColumns.Stars(1)           ),
+                    ( Row.TimerDisplay, GridRowsColumns.Stars(2)     ),
+                    ( Row.Island, GridRowsColumns.Stars(3)           ),
+                    ( Row.TimerButtons, GridRowsColumns.Stars(1)     ),
+                    ( Row.BottomWhiteSpace, GridRowsColumns.Stars(1) )
+                    ),
+                ColumnDefinitions = GridRowsColumns.Columns.Define(
+                    (Column.LeftTimerButton, GridRowsColumns.Stars(1)),
+                    (Column.TimerAmount, GridRowsColumns.Stars(2)),
+                    (Column.RightTimerButton, GridRowsColumns.Stars(1))
+                    ),
+                BackgroundColor = Color.FromArgb("BBD0FF"),
                 Children =
                 {
                     new Button
@@ -22,10 +40,23 @@ namespace FocusApp.Views
                         FontSize = 40,
                         BackgroundColor = Colors.Transparent
                     }
+                    .Row(Row.TopBar)
                     .Top()
                     .Left()
                     .Invoke(b => b.Clicked += (sender, e) => { Content = new SettingsView(); }),
-                    
+
+                    new Button
+                    {
+                        Text = SolidIcons.ChevronUp,
+                        BackgroundColor = Colors.Transparent,
+                        TextColor = Colors.Black,
+                        FontSize = 30,
+                    }
+                    .Font(family: nameof(SolidIcons), size: 40)
+                    .End()
+                    .CenterVertical()
+                    .Row(Row.TimerButtons)
+                    .Column(Column.LeftTimerButton),
 
                     new Label
                     {
@@ -34,6 +65,20 @@ namespace FocusApp.Views
                         HorizontalOptions = LayoutOptions.Center,
                         VerticalOptions = LayoutOptions.Center
                     }
+                    .Row(Row.TimerDisplay)
+                    .Column(Column.TimerAmount),
+
+                    new Button
+                    {
+                        Text = SolidIcons.ChevronDown,
+                        BackgroundColor = Colors.Transparent,
+                        TextColor = Colors.Black,
+                    }
+                    .Font(family: nameof(SolidIcons), size: 40)
+                    .Start()
+                    .CenterVertical()
+                    .Row(Row.TimerButtons)
+                    .Column(Column.RightTimerButton),
                 }
             };
         }
