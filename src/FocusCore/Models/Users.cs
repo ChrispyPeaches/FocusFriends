7bf3315cbea57341403af12cae2b59b9fd234135
JohnParks032 <80427077+JohnParks032@users.noreply.github.com>
2024-01-29 16:53:18 -0600

    Added nuget packages and created user model

---
+++ src/FocusCore/Models/Users.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FocusCore.Models;
+
+public class Users
+{
+	public Guid UserId { get; set; }
+	public string Email { get; set; } = null!;
+	public DateTime DateCreated { get; set; }
+	public int Balance { get; set; }
+	public string? FirstName { get; set; }
+	public string? LastName { get; set; }
+	public string? Pronouns { get; set; }
+	public byte[]? ProfilePicture { get; set; }
+
+}
+
