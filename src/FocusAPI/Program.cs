27efae87fe2feb01efe0fdd0254f9ae561633c20
ChrispyPeaches <gameboy9478@gmail.com>
2024-02-25 13:28:22 -0600

    Change ConnectionString for docker environment compatibility

--- src/FocusAPI/Program.cs
+++ src/FocusAPI/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddValidatorsFromAssembly(Assembly.GetAssembly(typeof(CreateUse
 // Register DbContext
 builder.Services.AddDbContext<FocusContext>(options =>
     {
-        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+        options.UseSqlServer(builder.Configuration["DefaultConnectionString"]);
     });
 
 var app = builder.Build();
