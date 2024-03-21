3688d5713b0989d7ba7b5a3abbc9e82e06464aff
Chris Perry <gameboy9478@gmail.com>
2024-03-21 13:18:23 -0500

    Add randomness to retrieving mindfulness tips by session rating

--- src/FocusApp.Client/Methods/MindfulnessTip/GetMindfulnessTipByRatingLevel.cs
+++ src/FocusApp.Client/Methods/MindfulnessTip/GetMindfulnessTipByRatingLevel.cs
@@ -2,6 +2,8 @@
 using FocusApp.Shared.Data;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Threading;
 using static FocusCore.Extensions.MindfulnessTipExtensions;
 
 namespace FocusApp.Client.Methods.MindfulnessTip;
@@ -25,18 +27,53 @@ public class GetMindfulnessTipByRatingLevel
 
     public class Handler : IRequestHandler<Query, Shared.Models.MindfulnessTip?>
     {
-        FocusAppContext _context;
-        public Handler(FocusAppContext context)
+        private readonly FocusAppContext _context;
+        private readonly ILogger<Handler> _logger;
+
+        public Handler(FocusAppContext context, ILogger<Handler> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         public async Task<Shared.Models.MindfulnessTip?> Handle(Query query, CancellationToken cancellationToken)
         {
-            var resultingTip = await _context.MindfulnessTips
-                .FirstOrDefaultAsync(tip => tip.SessionRatingLevel == query.RatingLevel.ToString(), cancellationToken);
+            Shared.Models.MindfulnessTip? resultingTip = null;
+            try
+            {
+                int indexOfTipToRetrieve = await GetRandomTipIndex(query, cancellationToken);
+
+                resultingTip = await GetTipByIndex(query, indexOfTipToRetrieve, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error when getting mindfulness tip by rating level");
+            }
 
             return resultingTip;
+        }
+
+        private async Task<int> GetRandomTipIndex(Query query, CancellationToken cancellationToken)
+        {
+            int randomInt = new Random().Next();
+
+            int numberOfTips = await _context.MindfulnessTips
+                .Where(tip => tip.SessionRatingLevel == query.RatingLevel.ToString())
+                .CountAsync(cancellationToken);
+
+            return randomInt % numberOfTips;
+
+        }
+
+        private async Task<Shared.Models.MindfulnessTip?> GetTipByIndex(
+            Query query,
+            int indexOfTipToRetrieve,
+            CancellationToken cancellationToken)
+        {
+            return await _context.MindfulnessTips
+                .Where(tip => tip.SessionRatingLevel == query.RatingLevel.ToString())
+                .Skip(indexOfTipToRetrieve)
+                .FirstOrDefaultAsync(cancellationToken);
         }
     }
 }
