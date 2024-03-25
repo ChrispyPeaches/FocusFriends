e4ade83b9fb9b44e6d96dadae8b611f075901274
egray <evangray7373@gmail.com>
2024-03-25 16:54:40 -0700

    Add point text, refactor grid creation

--- src/FocusApp.Client/Views/Social/LeaderboardsPage.cs
+++ src/FocusApp.Client/Views/Social/LeaderboardsPage.cs
@@ -19,9 +19,11 @@ namespace FocusApp.Client.Views.Social
         enum PageColumn { DailyLeadboardButton, WeeklyLeaderboardButton }
 
         enum TopThreeRow { TopProfilePicture, GoldPillar, SilverPillar, BronzePillar }
-        enum TopThreeColumn { Left, Center, Right}
+        enum TopThreeColumn { Left, Center, Right }
         public LeaderboardsPage()
         {
+            Grid TopThreeFriendsGrid = GetTopThreeFriendsGrid();
+
             Content = new Grid
             {
                 RowDefinitions = GridRowsColumns.Rows.Define(
@@ -97,99 +99,7 @@ namespace FocusApp.Client.Views.Social
                     .Column(PageColumn.WeeklyLeaderboardButton),
 
                     // Top Three Friends Leaderboard Display
-                    new Grid
-                    {
-                        RowDefinitions = GridRowsColumns.Rows.Define(
-                            (TopThreeRow.TopProfilePicture, GridRowsColumns.Stars(1)),
-                            (TopThreeRow.GoldPillar, GridRowsColumns.Stars(1)),
-                            (TopThreeRow.SilverPillar, GridRowsColumns.Stars(1)),
-                            (TopThreeRow.BronzePillar, GridRowsColumns.Stars(1))
-                            ),
-                        ColumnDefinitions = GridRowsColumns.Columns.Define(
-                            (TopThreeColumn.Left, GridRowsColumns.Stars(1)),
-                            (TopThreeColumn.Center, GridRowsColumns.Stars(1)),
-                            (TopThreeColumn.Right, GridRowsColumns.Stars(1))
-                            ),
-                        Children = 
-                        {
-                            // Bronze Pillar
-                            new Frame
-                            { 
-                                BackgroundColor = Colors.RosyBrown
-                            }
-                            .Row(TopThreeRow.BronzePillar)
-                            .Column(TopThreeColumn.Left),
-
-                            // Third Place Friend
-                            new Image
-                            {
-                                Source = new FileImageSource
-                                {
-                                    // Add logic that gets profile picture from user data
-                                    File = "dotnet_bot.png"
-                                },
-                            }
-                            // Note: Center denotes where within the grid cell the image will be centered
-                            //       Find a way to do this programatically
-                            .Clip(new EllipseGeometry { Center = new Point(64, 35), RadiusX = 27, RadiusY = 27 })
-                            .Row(TopThreeRow.SilverPillar)
-                            .Column(TopThreeColumn.Left)
-                            .CenterHorizontal()
-                            .CenterVertical(),
-
-                            // Silver Pillar
-                            new Frame
-                            {
-                                BackgroundColor = Colors.Silver
-                            }
-                            .Row(TopThreeRow.SilverPillar)
-                            .Column(TopThreeColumn.Right)
-                            .RowSpan(2),
-
-                            // Second Place Friend
-                            new Image
-                            {
-                                Source = new FileImageSource
-                                {
-                                    // Add logic that gets profile picture from user data
-                                    File = "dotnet_bot.png"
-                                },
-                            }
-                            // Note: Center denotes where within the grid cell the image will be centered
-                            //       Find a way to do this programatically
-                            .Clip(new EllipseGeometry { Center = new Point(64, 35), RadiusX = 27, RadiusY = 27 })
-                            .Row(TopThreeRow.GoldPillar)
-                            .Column(TopThreeColumn.Right)
-                            .CenterHorizontal()
-                            .CenterVertical(),
-
-                            // Gold Pillar
-                            new Frame
-                            {
-                                BackgroundColor = Colors.Gold
-                            }
-                            .Row(TopThreeRow.GoldPillar)
-                            .Column(TopThreeColumn.Center)
-                            .RowSpan(3),
-
-                            // First Place Friend
-                            new Image
-                            {
-                                Source = new FileImageSource
-                                {
-                                    // Add logic that gets profile picture from user data
-                                    File = "dotnet_bot.png"
-                                },
-                            }
-                            // Note: Center denotes where within the grid cell the image will be centered
-                            //       Find a way to do this programatically
-                            .Clip(new EllipseGeometry { Center = new Point(64, 35), RadiusX = 27, RadiusY = 27 })
-                            .Row(TopThreeRow.TopProfilePicture)
-                            .Column(TopThreeColumn.Center)
-                            .CenterHorizontal()
-                            .CenterVertical(),
-                        }
-                    }
+                    TopThreeFriendsGrid
                     .Row(PageRow.TopThreeFriendsDisplay)
                     .ColumnSpan(typeof(PageColumn).GetEnumNames().Length),
 
@@ -203,6 +113,136 @@ namespace FocusApp.Client.Views.Social
                 }
             };
         }
+
+        // TODO: Make this dynamic
+        private Grid GetTopThreeFriendsGrid()
+        {
+            // Top Three Friends Leaderboard Display
+            Grid topThreeFriendsGrid = new Grid
+            {
+                RowDefinitions = GridRowsColumns.Rows.Define(
+                    (TopThreeRow.TopProfilePicture, GridRowsColumns.Stars(1)),
+                    (TopThreeRow.GoldPillar, GridRowsColumns.Stars(1)),
+                    (TopThreeRow.SilverPillar, GridRowsColumns.Stars(1)),
+                    (TopThreeRow.BronzePillar, GridRowsColumns.Stars(1))
+                    ),
+                ColumnDefinitions = GridRowsColumns.Columns.Define(
+                    (TopThreeColumn.Left, GridRowsColumns.Stars(1)),
+                    (TopThreeColumn.Center, GridRowsColumns.Stars(1)),
+                    (TopThreeColumn.Right, GridRowsColumns.Stars(1))
+                    ),
+                Children =
+                {
+                    // Bronze Pillar
+                    new Frame
+                    {
+                        BackgroundColor = Colors.RosyBrown,
+                        Content = 
+                            // Third Place Points
+                            new Label
+                            {
+                                Text = "10"
+                            }
+                            .Row(TopThreeRow.BronzePillar)
+                            .Column(TopThreeColumn.Left)
+                            .CenterHorizontal()
+                    }
+                    .Row(TopThreeRow.BronzePillar)
+                    .Column(TopThreeColumn.Left),
+
+                    // Third Place Friend
+                    new Image
+                    {
+                        Source = new FileImageSource
+                        {
+                            // Add logic that gets profile picture from user data
+                            File = "dotnet_bot.png"
+                        },
+                    }
+                    // Note: Center denotes where within the grid cell the image will be centered
+                    //       Find a way to do this programatically
+                    .Clip(new EllipseGeometry { Center = new Point(64, 35), RadiusX = 27, RadiusY = 27 })
+                    .Row(TopThreeRow.SilverPillar)
+                    .Column(TopThreeColumn.Left)
+                    .CenterHorizontal()
+                    .CenterVertical(),
+
+                    // Silver Pillar
+                    new Frame
+                    {
+                        BackgroundColor = Colors.Silver,
+                        Content =
+                            // Second Place Points
+                            new Label
+                            {
+                                Text = "20"
+                            }
+                            .Row(TopThreeRow.SilverPillar)
+                            .Column(TopThreeColumn.Right)
+                            .CenterHorizontal()
+                    }
+                    .Row(TopThreeRow.SilverPillar)
+                    .Column(TopThreeColumn.Right)
+                    .RowSpan(2),
+
+                    // Second Place Friend
+                    new Image
+                    {
+                        Source = new FileImageSource
+                        {
+                            // Add logic that gets profile picture from user data
+                            File = "dotnet_bot.png"
+                        },
+                    }
+                    // Note: Center denotes where within the grid cell the image will be centered
+                    //       Find a way to do this programatically
+                    .Clip(new EllipseGeometry { Center = new Point(64, 35), RadiusX = 27, RadiusY = 27 })
+                    .Row(TopThreeRow.GoldPillar)
+                    .Column(TopThreeColumn.Right)
+                    .CenterHorizontal()
+                    .CenterVertical(),
+
+                    // Gold Pillar
+                    new Frame
+                    {
+                        BackgroundColor = Colors.Gold,
+                        Content =                             
+                            // First Place Points
+                            new Label
+                            {
+                                Text = "30",
+                                TextColor = Colors.Black
+                            }
+                            .Row(TopThreeRow.GoldPillar)
+                            .Column(TopThreeColumn.Center)
+                            .CenterHorizontal()
+                    }
+                    .Row(TopThreeRow.GoldPillar)
+                    .Column(TopThreeColumn.Center)
+                    .RowSpan(3),
+
+                    // First Place Friend
+                    new Image
+                    {
+                        Source = new FileImageSource
+                        {
+                            // Add logic that gets profile picture from user data
+                            File = "dotnet_bot.png"
+                        },
+                    }
+                    // Note: Center denotes where within the grid cell the image will be centered
+                    //       Find a way to do this programatically
+                    .Clip(new EllipseGeometry { Center = new Point(64, 35), RadiusX = 27, RadiusY = 27 })
+                    .Row(TopThreeRow.TopProfilePicture)
+                    .Column(TopThreeColumn.Center)
+                    .CenterHorizontal()
+                    .CenterVertical(),
+                }
+            };
+
+            return topThreeFriendsGrid;
+        }
+
         private async void BackButtonClicked(object sender, EventArgs e)
         {
             // Back navigation reverses animation so can keep right to left
