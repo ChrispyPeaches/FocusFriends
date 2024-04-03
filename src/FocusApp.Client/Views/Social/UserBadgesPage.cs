79f6a1f59fd79b6f17c5abbd5f8c4e6ee67ce993
JohnParks032 <80427077+JohnParks032@users.noreply.github.com>
2024-04-03 00:59:19 -0500

    Created basic UserBadgesPage

---
+++ src/FocusApp.Client/Views/Social/UserBadgesPage.cs
@@ -0,0 +1,38 @@
+﻿using FocusApp.Client.Clients;
+using FocusApp.Client.Helpers;
+using FocusApp.Client.Resources;
+using FocusApp.Shared.Data;
+
+
+namespace FocusApp.Client.Views.Social;
+
+internal class UserBadgesPage : BasePage
+{
+    IAPIClient _client;
+    IAuthenticationService _authenticationService;
+    FocusAppContext _focusAppContext;
+    Helpers.PopupService _popupService;
+
+
+    #region Frontend
+    public UserBadgesPage(IAPIClient client, IAuthenticationService authenticationService, Helpers.PopupService popupService, FocusAppContext localContext)
+    {
+        _client = client;
+        _authenticationService = authenticationService;
+        _popupService = popupService;
+        _focusAppContext = localContext;
+
+        Content = new FlexLayout
+        {
+            BackgroundColor = AppStyles.Palette.FairyTale,
+            Children =
+            {
+
+            }
+        };
+    }
+    #endregion
+
+    #region Backend
+    #endregion
+}
