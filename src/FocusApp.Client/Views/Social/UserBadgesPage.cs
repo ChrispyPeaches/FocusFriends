e32cd5fe7b041be3082005c098fc8e6227a05c35
JohnParks032 <80427077+JohnParks032@users.noreply.github.com>
2024-04-03 01:05:18 -0500

    Added to backend of UserBadgesPage

--- src/FocusApp.Client/Views/Social/UserBadgesPage.cs
+++ src/FocusApp.Client/Views/Social/UserBadgesPage.cs
@@ -1,7 +1,9 @@
 ﻿using FocusApp.Client.Clients;
 using FocusApp.Client.Helpers;
 using FocusApp.Client.Resources;
+using FocusApp.Client.Views.Shop;
 using FocusApp.Shared.Data;
+using FocusApp.Shared.Models;
 
 
 namespace FocusApp.Client.Views.Social;
@@ -34,5 +36,24 @@ internal class UserBadgesPage : BasePage
     #endregion
 
     #region Backend
+
+    protected override async void OnAppearing()
+    {
+        // Theoretically a user shouldnt be able to get here, but added for redundancy
+        if (_authenticationService.CurrentUser == null)
+        {
+            var loginPopup = (EnsureLoginPopupInterface)_popupService.ShowAndGetPopup<EnsureLoginPopupInterface>();
+            loginPopup.OriginPage = nameof(ShopPage);
+        }
+
+        // TODO: Add logic to fetch from local db
+
+        base.OnAppearing();
+    }
+
+    private List<UserBadge> GetLocalBadges()
+    {
+        return null;
+    }
     #endregion
 }
