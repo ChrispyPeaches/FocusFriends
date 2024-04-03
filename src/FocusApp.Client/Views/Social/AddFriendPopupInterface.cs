f52e0173ddf3391ea6e45f17a81b56929463f3b2
Eli Payton <80854811+wesleypayton@users.noreply.github.com>
2024-04-03 00:44:53 -0500

    Work to Popup and new model

--- src/FocusApp.Client/Views/Social/AddFriendPopupInterface.cs
+++ src/FocusApp.Client/Views/Social/AddFriendPopupInterface.cs
@@ -1,7 +1,11 @@
-﻿using CommunityToolkit.Maui.Markup;
+﻿using CommunityToolkit.Maui.Converters;
+using CommunityToolkit.Maui.Markup;
 using CommunityToolkit.Maui.Markup.LeftToRight;
 using CommunityToolkit.Maui.Views;
+using FocusApp.Client.Clients;
 using FocusApp.Client.Resources;
+using FocusApp.Shared.Models;
+using FocusCore.Models;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Controls.Shapes;
 using Microsoft.Maui.Graphics.Text;
@@ -10,39 +14,27 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.Tracing;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
+using FriendRequest = FocusApp.Shared.Models.FriendRequest;
 
 namespace FocusApp.Client.Views.Social
 {
     internal class AddFriendPopupInterface : BasePopup
     {
-        private Helpers.PopupService _popupService;
+        IAPIClient _client;
+        Helpers.PopupService _popupService;
+        ListView _friendrequestView { get; set; }
 
-        public AddFriendPopupInterface(Helpers.PopupService popupService)
+        public AddFriendPopupInterface(IAPIClient client, Helpers.PopupService popupService)
         {
+            _client = client;
             _popupService = popupService;
 
+            _friendrequestView = BuildFriendRequestListView();
+
             Color = Colors.Transparent;
-
-            List<ImageCell> pendingFriends = new List<ImageCell>();
-            for (int i = 0; i < 5; i++)
-            {
-                pendingFriends.Add(new ImageCell
-                {
-                    Text = "Friend " + i,
-                    ImageSource = new FileImageSource
-                    {
-                        // Add logic that gets profile picture from pending friend user data
-                        File = "dotnet_bot.png"
-                    },
-                    BindingContext = this
-                });
-            };
-
-            DataTemplate pendingFriendDataTemplate = new DataTemplate(typeof(ImageCell));
-            pendingFriendDataTemplate.SetBinding(ImageCell.TextProperty, "Text");
-            pendingFriendDataTemplate.SetBinding(ImageCell.ImageSourceProperty, "ImageSource");
 
             Content = new Border
             {
@@ -89,14 +81,29 @@ namespace FocusApp.Client.Views.Social
                             WidthRequest = 360,
                             HeightRequest = 100,
                             BackgroundColor = Colors.Transparent,
-                            Content = new Entry
+                            Content = new HorizontalStackLayout
                             {
-                                Placeholder = "Enter friend's username",
-                                FontSize = 20,
-                                TextColor = Colors.Black,
-                                ClearButtonVisibility = ClearButtonVisibility.Never,
-                                Keyboard = Keyboard.Plain
-                            }.Fill()
+                                Children =
+                                {
+                                    new Entry
+                                    {
+                                        Placeholder = "Enter friend's username",
+                                        FontSize = 20,
+                                        TextColor = Colors.Black,
+                                        ClearButtonVisibility = ClearButtonVisibility.Never,
+                                        Keyboard = Keyboard.Plain
+                                    }
+                                    .Fill(),
+
+                                    new Button
+                                    {
+                                        Text = "Send",
+                                        WidthRequest = 100,
+                                        HeightRequest = 50,
+                                        FontSize = 20
+                                    }
+                                }
+                            }
                         },
 
                         // Horizontal Divider
@@ -113,18 +120,111 @@ namespace FocusApp.Client.Views.Social
                             WidthRequest = 360,
                             HeightRequest = 350,
                             BackgroundColor = Colors.Transparent,
-                            Content = new ListView
+                            Content = _friendrequestView
+                            
+                            /*new ListView
                             {
                                 Header = "Pending Friend Requests",
                                 ItemsSource = pendingFriends,
                                 ItemTemplate = pendingFriendDataTemplate
-                            }
+                            }*/
                         },
                     }
                 }
                 .Top()
                 .Right()
             };
+
+            // Populate PendingFriends upon popup open
+            PopulatePopup();
+        }
+
+        private ListView BuildFriendRequestListView()
+        {
+            ListView listView = new ListView();
+            listView.Header = "Pending Friend Requests";
+
+
+            listView.ItemTemplate = new DataTemplate(() =>
+            {
+                ViewCell cell = new ViewCell();
+                HorizontalStackLayout stackLayout = new HorizontalStackLayout();
+
+                /*// Friend profile picture
+                Image friendImage = new Image
+                {
+                };
+                friendImage.SetBinding(
+                    Image.SourceProperty, "ImageSource",
+                    converter: new ByteArrayToImageSourceConverter());*/
+
+                // Friend username
+                Label friendUsername = new Label
+                {
+                    FontSize = 20
+                };
+                friendUsername.SetBinding(Label.TextProperty, "FriendUserName");
+
+                // Accept button
+                Button buttonAccept = new Button
+                {
+                    Text = "Accept",
+                    WidthRequest = 80,
+                    HeightRequest = 40,
+                    FontSize = 15,
+                    BackgroundColor = Colors.Green
+                };
+                buttonAccept.HorizontalOptions = LayoutOptions.End;
+
+                // Reject Button
+                Button buttonReject = new Button
+                {
+                    Text = "Reject",
+                    WidthRequest = 80,
+                    HeightRequest = 40,
+                    FontSize = 15,
+                    BackgroundColor = Colors.Red
+                };
+                buttonReject.HorizontalOptions = LayoutOptions.End;
+
+                //stackLayout.Children.Add(friendImage);
+                stackLayout.Children.Add(friendUsername);
+                stackLayout.Children.Add(buttonAccept);
+                stackLayout.Children.Add(buttonReject);
+                cell.View = stackLayout;
+
+                return cell;
+            });
+
+            return listView;
+        }
+
+        private List<FriendRequest> seedFakeFriendRequests()
+        {
+            List<FriendRequest> pendingFriends = new List<FriendRequest>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                FriendRequest fakeFriend = new FriendRequest
+                {
+                    FriendUserName = "Test" + i,
+                    FriendEmail = "Test" + i,
+                    FriendStatus = 0,
+                    userInitiated = true
+                };
+                pendingFriends.Add(fakeFriend);
+            };
+
+            return pendingFriends;
+        }
+
+        public void PopulatePopup()
+        {
+            List<FriendRequest> pendingFriendRequests;
+            pendingFriendRequests = seedFakeFriendRequests();
+            //pendingFriendRequests = await _client.
+
+            _friendrequestView.ItemsSource = pendingFriendRequests;
         }
     }
 }
---
+++ src/FocusCore/Models/FriendRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FocusCore.Models
+{
+    public class FriendRequest
+    {
+        public Guid FriendId { get; set; }
+        public string FriendUserName { get; set; }
+        public required string FriendEmail { get; set; } = null!;
+        public byte[] FriendProfilePicture { get; set; }
+        public int FriendStatus { get; set; }
+
+        // True if User is Inviter
+        public bool userInitiated { get; set; }
+    }
+}
