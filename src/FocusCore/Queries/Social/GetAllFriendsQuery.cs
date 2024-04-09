bde057f4aac6056f6f932fa31aadc89d15c909b2
Eli Payton <elipayton1@gmail.com>
2024-04-09 02:59:16 -0500

    Create GetAllFriendsQuery.cs

---
+++ src/FocusCore/Queries/Social/GetAllFriendsQuery.cs
@@ -0,0 +1,10 @@
+﻿using FocusCore.Models;
+using FocusApp.Shared.Models;
+using MediatR;
+
+namespace FocusCore.Queries.Social;
+public class GetAllFriendsQuery : IRequest<List<FriendShip>>
+{ 
+    public Guid UserId { get; set; }
+}
+
